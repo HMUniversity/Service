88f3731ba6391215823a372c47eacccd73563b6b
JiJi <i@mmdjiji.com>
2023-09-14 16:59:43 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Home University Service System is coming...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/HMUniversity/About"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Home University
         </a>
       </header>
     </div>
